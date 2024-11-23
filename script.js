d2a61b21b80c48c81abd495f1246f2f000a25c1a
V1RU5 <79205320+jodri-code@users.noreply.github.com>
2024-11-23 18:17:31 +0100

    Add a simple explanation + example usage

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ window.onload = function(){
     const src = fragment.get('vid');
 
     if(src != null){
-        image.innerHTML = `<img src="https://i.ytimg.com/vi/${src}/hqdefault.jpg" style="border-radius: 4px; width: 100%;">`
+        image.innerHTML = `<img src="https://i.ytimg.com/vi/${src}/hqdefault.jpg" style="border-radius: 4px; width: 100%;">`;
     } else{
-        window.alert('no vid specified')
+        image.innerHTML = `<p>This <b>thing</b> gets thumbnails from yt videos using the i.ytimg.com domain's /vi endpoint.</p><p>Append "/#vid={videoId}" where {videoId} is the ID of a video.<br><h2>Example usage:</h2><p>"youtube.com/watch?v=_XnhRzRFbLU" turns into "common-codes.github.io/rip.ytimg/#vid=_XnhRzRFbLU".<br><b>PLEASE NOTE:</b> Most times you will have to refresh the tab!</p>`;
     }
 }
