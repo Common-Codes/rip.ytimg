2737c42fb7baea036bea855edf35a20ad5a01cbe
V1RU5 <79205320+jodri-code@users.noreply.github.com>
2026-04-23 21:46:08 +0200

    Update script.js
    Made default thumbnail resolution 720p with a 180p fallback mode for older videos

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ window.onload = function(){
     const src = params.get('vid') ?? fragment.get('vid');
 
     if(src != null){
-        image.innerHTML = `<img src="https://i.ytimg.com/vi/${src}/hqdefault.jpg" style="border-radius: 4px; width: 100%;">`;
+        image.innerHTML = `<img src="https://i.ytimg.com/vi/${src}/maxresdefault.jpg" onerror="this.src='https://i.ytimg.com/vi/${src}/mqdefault.jpg'" style="border-radius: 4px; width: 100%;">`;
     } else{
         image.innerHTML = `<p>This <b>thing</b> gets thumbnails from yt videos using the i.ytimg.com domain's /vi endpoint.</p><p>Append "/#vid={videoId}" where {videoId} is the ID of a video.<br><h2>Example usage:</h2><p>"youtube.com/watch?v=_XnhRzRFbLU" turns into "common-codes.github.io/rip.ytimg/#vid=_XnhRzRFbLU".<br><b>PLEASE NOTE:</b> Most times you will have to refresh the tab!</p>`;
     }
