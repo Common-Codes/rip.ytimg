091998c13f6f7f3fc05696a20d31b2757b24b090
V1RU5 <79205320+jodri-code@users.noreply.github.com>
2026-04-24 18:44:08 +0200

    Make it actually graciously fall back

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
 window.onload = function(){
-    const image = document.getElementById("image");
+    const imageContainer = document.getElementById("image_container");
     const params = new URLSearchParams(window.location.search.slice(1));
     const fragment = new URLSearchParams(window.location.hash.slice(1));
     const src = params.get('vid') ?? fragment.get('vid');
 
     if(src != null){
-        image.innerHTML = `<img src="https://i.ytimg.com/vi/${src}/maxresdefault.jpg" onerror="this.src='https://i.ytimg.com/vi/${src}/mqdefault.jpg'" style="border-radius: 4px; width: 100%;">`;
+        const img = document.createElement('img');
+        img.style.cssText = 'border-radius: 6px; width: 100%;'
+        
+        img.onload = function() {
+            if(img.width == 120) {
+                img.src = `https://i.ytimg.com/vi/${src}/mqresdefault.jpg`
+            }
+        }
+
+        img.src = `https://i.ytimg.com/vi/${src}/maxresdefault.jpg`;
+        imageContainer.appendChild(img);
     } else{
-        image.innerHTML = `<p>This <b>thing</b> gets thumbnails from yt videos using the i.ytimg.com domain's /vi endpoint.</p><p>Append "/#vid={videoId}" where {videoId} is the ID of a video.<br><h2>Example usage:</h2><p>"youtube.com/watch?v=_XnhRzRFbLU" turns into "common-codes.github.io/rip.ytimg/#vid=_XnhRzRFbLU".<br><b>PLEASE NOTE:</b> Most times you will have to refresh the tab!</p>`;
+        imageContainer.innerHTML = `<p>This <b>thing</b> gets thumbnails from yt videos using the i.ytimg.com domain's /vi endpoint.</p><p>Append "/?vid={videoId}" where {videoId} is the ID of a video.<br><h2>Example usage:</h2><p>"youtube.com/watch?v=_XnhRzRFbLU" turns into "common-codes.github.io/rip.ytimg/?vid=_XnhRzRFbLU".<br><b>PLEASE NOTE:</b> Most times you will have to refresh the tab!</p>`;
     }
 }
